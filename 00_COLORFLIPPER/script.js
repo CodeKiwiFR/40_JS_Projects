de1c15ed5d8f989e5bd7e7223c277c1f3c191ff6
CodeKiwiFR <madric.hottinger.101@gmail.com>
2022-08-10 23:46:41 +0200

    Project 00: finishing style - Adding script to manage random bg color

--- 00_COLORFLIPPER/script.js
+++ 00_COLORFLIPPER/script.js
@@ -1,1 +1,46 @@
-console.log('test');
+/* ************************************************** */
+/* Function definitions */
+/* ************************************************** */
+// Get a random hex. number between 0 and 255
+const getRandomHexColorValue = () => {
+    let value, hexColorValue;
+
+    value = Math.floor(Math.random() * 255);
+    hexColorValue = value.toString(16);
+
+    if (hexColorValue.length === 1) {
+        hexColorValue = '0' + hexColorValue;
+    }
+
+    return hexColorValue.toUpperCase();
+};
+
+// Generating a random haxadecimal color value
+const colorRandomiser = () => {
+    return (
+        '#' +
+        getRandomHexColorValue() +
+        getRandomHexColorValue() +
+        getRandomHexColorValue()
+    );
+};
+
+// Changes mainContent background color and puts the color hex. value in colorNamePanel
+const colorSwitcherButtonHandler = (event) => {
+    const newColor = colorRandomiser();
+
+    mainContent.style.backgroundColor = newColor;
+    colorNamePanel.textContent = newColor;
+};
+
+/* ************************************************** */
+/* Variable definitions */
+/* ************************************************** */
+const colorSwitcherButton = document.getElementById('colorSwitcherButton');
+const mainContent = document.getElementById('mainContent');
+const colorNamePanel = document.getElementById('colorNamePanel');
+
+/* ************************************************** */
+/* Main program */
+/* ************************************************** */
+colorSwitcherButton.addEventListener('click', colorSwitcherButtonHandler);
