939074b67595c07c2081da9120f511cbd49a4238
CodeKiwiFR <madric.hottinger.101@gmail.com>
2022-08-22 23:24:36 +0200

    Project 03: mobile style ok - basic menu script written

--- 03_NAVBAR/script.js
+++ 03_NAVBAR/script.js
@@ -1,15 +1,29 @@
 /* ************************************************** */
-/* Class definitions */
-/* ************************************************** */
-
-/* ************************************************** */
 /* Variable definitions */
 /* ************************************************** */
+const mobileButtonElement = document.getElementById('mobileButton');
+const backdropElement = document.getElementById('backdrop');
+const menuContainerElement = document.getElementById('menuContainer');
+
+const elementsToToggle = [mobileButtonElement, backdropElement, menuContainerElement];
 
 /* ************************************************** */
 /* Function definitions */
 /* ************************************************** */
+const mobileButtonHandler = (event) => {
+    for (const element of elementsToToggle) {
+        element.classList.toggle('OpenedMenu');
+    }
+};
+
+const backdropHandler = (event) => {
+    for (const element of elementsToToggle) {
+        element.classList.remove('OpenedMenu');
+    }
+}
 
 /* ************************************************** */
 /* Main program */
 /* ************************************************** */
+mobileButtonElement.addEventListener('click', mobileButtonHandler);
+backdropElement.addEventListener('click', backdropHandler);
