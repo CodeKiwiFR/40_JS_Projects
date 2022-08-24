dc9320d2b0b79592b47bf837f86f1a9f21dace41
CodeKiwiFR <madric.hottinger.101@gmail.com>
2022-08-25 00:49:23 +0200

    Project 04: Basic js script to call random user api - adding modal/backdrop and styling them

--- 04_MODAL/script.js
+++ 04_MODAL/script.js
@@ -5,11 +5,34 @@
 /* ************************************************** */
 /* Variable definitions */
 /* ************************************************** */
+const randomButtonElement = document.getElementById('randomButton');
+const apiURL = 'https://random-data-api.com/api/v2/users';
 
 /* ************************************************** */
 /* Function definitions */
 /* ************************************************** */
+const randomButtonHandler = (event) => {
+    fetch(apiURL)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw 'Request failed';
+            }
+        })
+        .then((response) => {
+            console.log(response);
+            console.log(response.avatar);
+            console.log(response.first_name);
+            console.log(response.last_name);
+            console.log(response.email);
+            console.log(response.employment.title);
+            console.log(response.phone_number);
+        })
+        .catch((error) => console.log('Error: ' + error));
+};
 
 /* ************************************************** */
 /* Main program */
 /* ************************************************** */
+randomButtonElement.addEventListener('click', randomButtonHandler);
