50e4a22003fb96406cf9b611ebad0db1cd78a093
CodeKiwiFR <madric.hottinger.101@gmail.com>
2022-08-26 01:34:50 +0200

    Project 04: Script updates user card - Small adjustments of template and style

--- 04_MODAL/script.js
+++ 04_MODAL/script.js
@@ -1,17 +1,28 @@
 /* ************************************************** */
-/* Class definitions */
-/* ************************************************** */
-
-/* ************************************************** */
 /* Variable definitions */
 /* ************************************************** */
 const randomButtonElement = document.getElementById('randomButton');
+const backdropElement = document.getElementById('backdrop');
+const personCardElement = document.getElementById('personCard');
+const personCardLoaderElement = personCardElement.querySelector('#personCardLoader');
+const personCardCloseButtonElement = personCardElement.querySelector('#personCardCloseButton');
+const personCardDetailsElement = personCardElement.querySelector('#personCardDetails');
+const personAvatarElement = personCardDetailsElement.querySelector('#personAvatar');
+const personNameElement = personCardDetailsElement.querySelector('#personName');
+const personEmailElement = personCardDetailsElement.querySelector('#personEmail');
+const personJobElement = personCardDetailsElement.querySelector('#personJob');
+const personPhoneElement = personCardDetailsElement.querySelector('#personPhone');
+
 const apiURL = 'https://random-data-api.com/api/v2/users';
 
 /* ************************************************** */
 /* Function definitions */
 /* ************************************************** */
 const randomButtonHandler = (event) => {
+    for (const element of [personCardElement, backdropElement]) {
+        element.classList.add('modalOpened');
+    }
+
     fetch(apiURL)
         .then((response) => {
             if (response.status === 200) {
@@ -21,18 +32,31 @@ const randomButtonHandler = (event) => {
             }
         })
         .then((response) => {
-            console.log(response);
-            console.log(response.avatar);
-            console.log(response.first_name);
-            console.log(response.last_name);
-            console.log(response.email);
-            console.log(response.employment.title);
-            console.log(response.phone_number);
+            personAvatarElement.src = response.avatar;
+            personNameElement.textContent = response.first_name + ' ' + response.last_name;
+            personEmailElement.textContent = response.email;
+            personJobElement.textContent = response.employment.title;
+            personPhoneElement.textContent = response.phone_number;
+            
+            for (const element of [personCardDetailsElement, personCardLoaderElement]) {
+                element.classList.add('loadedUser');
+            }
         })
         .catch((error) => console.log('Error: ' + error));
 };
+
+const closeModalHandler = (event) => {
+    for (const element of [personCardElement, backdropElement]) {
+        element.classList.remove('modalOpened');
+    }
+    for (const element of [personCardDetailsElement, personCardLoaderElement]) {
+        element.classList.remove('loadedUser');
+    }
+}
 
 /* ************************************************** */
 /* Main program */
 /* ************************************************** */
 randomButtonElement.addEventListener('click', randomButtonHandler);
+backdropElement.addEventListener('click', closeModalHandler);
+personCardCloseButtonElement.addEventListener('click', closeModalHandler);
